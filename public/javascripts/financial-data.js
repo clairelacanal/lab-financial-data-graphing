8e40dd0f7c04693e9d482d41cd817804d4a26287
Claire Lacanal <lacanal.claire@gmail.com>
2020-09-15 21:24:51 +0200

    ite 2 done

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -5,9 +5,37 @@ axios
   .get(url)
   .then(response => {
     console.log(response.data)
+    printData(response.data)
   })
   .catch(err => console.log('Error while getting the data: ', err));
 
+
+  function printData(stockData) {
+    const numberData = stockData['bpi'];
+
+    const stockDates = Object.keys(numberData);
+    const stockPrices = stockDates.map(date => numberData[date]);
+
+    const ctx = document.getElementById('my-chart').getContext('2d');
+    const chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: stockDates,
+            datasets: [
+                {
+                    label: 'Financial Data Graph',
+                    backgroundColor: 'red',
+                    borderColor: 'yellow',
+                    data: stockPrices
+                }
+            ]
+        }
+    })
+
+  }
+
+  
+ 
 
 
  
