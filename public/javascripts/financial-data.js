3e635d6a830a9d1424fe84781cd9819ef7c1952a
Claire Lacanal <lacanal.claire@gmail.com>
2020-09-16 19:09:10 +0200

    ite 3 completely finish

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,46 +1,57 @@
 
 
+let startInitial = document.querySelector('#start');
+startInitial.value = '2016-01-01'
 
+let endInitial = document.querySelector('#end');
+endInitial.value = '2018-01-01'
 
-const getBitcoinInfo = () => {
-  let startDate = document.querySelector('#start').value;
-  let endDate = document.querySelector('#end').value;
+  const getBitcoinInfo = () => {
+    let startDate = document.querySelector('#start').value;
+    let endDate = document.querySelector('#end').value;
 
-  axios
-  .get(`https://api.coindesk.com/v1/bpi/historical/close.json$?start=${startDate}&end=${endDate}`)
-  .then(response => {
-    console.log(response.data)
-    printData(response.data)
-  })
-  .catch(err => console.log('Error while getting the data: ', err));
-
-
-  function printData(stockData) {
-    const numberData = stockData['bpi'];
-
-    const stockDates = Object.keys(numberData);
-    const stockPrices = stockDates.map(date => numberData[date]);
-
-    const ctx = document.getElementById('my-chart').getContext('2d');
-    const chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: stockDates,
-            datasets: [
-                {
-                    label: 'Financial Data Graph',
-                    backgroundColor: 'red',
-                    borderColor: 'yellow',
-                    data: stockPrices
-                }
-            ]
-        }
+    
+  
+    axios
+    .get(`https://api.coindesk.com/v1/bpi/historical/close.json$?start=${startDate}&end=${endDate}`)
+    .then(response => {
+      console.log(response.data)
+      printData(response.data)
     })
+    .catch(err => console.log('Error while getting the data: ', err));
 
+    
+  
+  
+    function printData(stockData) {
+      const numberData = stockData['bpi'];
+  
+      const stockDates = Object.keys(numberData);
+      const stockPrices = stockDates.map(date => numberData[date]);
+  
+      const ctx = document.getElementById('my-chart').getContext('2d');
+      const chart = new Chart(ctx, {
+          type: 'line',
+          data: {
+              labels: stockDates,
+              datasets: [
+                  {
+                      label: 'Financial Data Graph',
+                      backgroundColor: 'red',
+                      borderColor: 'yellow',
+                      data: stockPrices
+                  }
+              ]
+          }
+      })
+  
+    }
   }
-}
+  
+  getBitcoinInfo()
 
-getBitcoinInfo()
+
+
 
 document.getElementById('start').addEventListener("change", getBitcoinInfo);
 document.getElementById('end').addEventListener("change", getBitcoinInfo);
